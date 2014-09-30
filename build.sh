b9255e9f045ddb1005af7b9841fb97c105e49fd2
Ryan Rapp <ryan11429@gmail.com>
2014-09-29 17:37:05 -0700

    remove other dependencies

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+sudo apt-get install gfortran libopenblas-dev liblapack-dev
+pip install virtualenv
 cd appfiles
 virtualenv .
 source bin/activate
-sudo apt-get install gfortran libopenblas-dev liblapack-dev
 pip install numpy
 pip install scipy
 pip install -r dependencies.txt
