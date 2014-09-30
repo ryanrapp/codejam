355eb3eee5bb02ffe1107e998af026c04b6ab01e
Ryan Rapp <ryan11429@gmail.com>
2014-09-29 17:44:39 -0700

    use apt-get to install numpy etc

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get install gfortran libopenblas-dev liblapack-dev
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 pip install virtualenv
 cd appfiles
 virtualenv .
 source bin/activate
-pip install numpy
-pip install scipy
+
 pip install -r dependencies.txt
